091991b486adf89235b72e91b55e8da2321ffba3
Tim <chent1101@gmail.com>
2015-06-08 21:25:27 -0600

    Added models, views, and updated contact path in base.html

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import ContactForm
 
-# Register your models here.
+
+class ContactFormAdmin(admin.ModelAdmin):
+    class Meta:
+        mode = ContactForm
+
+admin.site.register(ContactForm, ContactFormAdmin)
+
--- django_ecommerce/urls.py
+++ django_ecommerce/urls.py
@@ -17,5 +17,6 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^pages/', include('django.contrib.flatpages.urls')),
+    url(r'^contact/', 'contact.views.contact', name='contact'),
 
 )
